bffb00a6e2bc4df9e427fe2c31a7bc4169b5acde
Sourav <dey.sourav777@gmail.com>
2024-12-27 15:44:37 -0500

    updated logic

--- otp_generator/otp_generator.py
+++ otp_generator/otp_generator.py
@@ -16,4 +16,11 @@ def generate_otp(length=6, numeric=True, alphabets=False, special_chars=False):
     if not char_set:
         raise ValueError("At least one option must be True.")
 
-    return ''.join(random.choices(char_set, k=length))
+    otp = ''
+    if numeric:
+        otp += random.choice('123456789')  # Ensure the first character is not 0
+        length -= 1
+
+    otp += ''.join(random.choices(char_set, k=length))
+    return ''.join(random.sample(otp, len(otp)))
+
